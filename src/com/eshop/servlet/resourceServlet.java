9510c97623bd46463f5856b4ec4a237f14d7dc84
longruchun <1430468717@qq.com>
2018-11-19 13:09:07 +0800

    q

--- src/com/eshop/servlet/resourceServlet.java
+++ src/com/eshop/servlet/resourceServlet.java
@@ -3,9 +3,12 @@ package com.eshop.servlet;
 import java.io.IOException;
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
 
 import com.eshop.biz.IshopMenuBiz;
 import com.eshop.biz.impl.shopMenuBizImpl;
@@ -14,11 +17,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebServlet("/resourceServlet")
+@Controller
 public class resourceServlet extends BaseServlet {
-	
+	//因为bean配置已经交给spring管理
 	public IshopMenuBiz biz;
 	
-  
+  @Resource
 
 	public void setBiz(IshopMenuBiz biz) {
 		this.biz = biz;
